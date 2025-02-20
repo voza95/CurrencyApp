1ccd25cdb436429168bdc1bbc5c8df0afd990533
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2025-02-20 23:17:21 +0530

    Added CurrencyApiService

---
+++ composeApp/src/commonMain/kotlin/org/voza/currencyexchange/data/remote/api/CurrencyApiService.kt
@@ -0,0 +1,8 @@
+package org.voza.currencyexchange.data.remote.api
+
+class CurrencyApiService {
+    companion object {
+        const val ENDPOINT = "https://api.currencyapi.com/v3/latest"
+        const val API_KEY = "API_KEY_HERE"
+    }
+}
